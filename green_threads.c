eba99e3ff506356abbd44fe1d728d1224df23a1f
dschinde <dschindelman@gmail.com>
2016-02-17 11:10:48 -0600

    Fixed compilation errors
    The original code didn't compile. This should.

--- green_threads.c
+++ green_threads.c
@@ -4,7 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static const int s_max_threads = 4;
+#define MAX_THREADS (4)
+
 static const int s_stack_size = 0x400000;
 
 struct green_thread {
@@ -24,7 +25,7 @@ struct green_thread {
 	} state;
 };
 
-struct green_thread gt_table[s_max_threads];
+struct green_thread gt_table[MAX_THREADS];
 struct green_thread *current_gt;
 
 void gt_init();
@@ -61,7 +62,7 @@ bool gt_schedule()
 	struct green_thread *next_gt = current_gt;
 	while (next_gt->state != Ready) {
 		next_gt++;
-		if (next_gt == &gt_table[s_max_threads]) {
+		if (next_gt == &gt_table[MAX_THREADS]) {
 			next_gt = &gt_table[0];
 		}
 		if (next_gt == current_gt) {
@@ -89,7 +90,7 @@ int gt_create(void (*function)())
 {
 	struct green_thread *p;
 	for (p = &gt_table[0];; p++) {
-		if (p == &gt_table[s_max_threads]) {
+		if (p == &gt_table[MAX_THREADS]) {
 			return -1;
 		} else if (p->state == Unused) {
 			break;
@@ -115,7 +116,7 @@ void do_work()
 
 	int id = ++x;
 	for (uint64_t i = 0; i < 10000000; i++) {
-		printf("%d %llu\n", id, i);
+		printf("%d %lu\n", id, i);
 		gt_schedule();
 	}
 }
